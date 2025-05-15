75c9adfeb9b58a8a825c5367f52bc228075008ec
Rafael Jaques <rafael@phpit.com.br>
2025-05-14 22:01:45 -0300

    Instalação do Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,31 @@
 <?php
 
-use App\Http\Controllers\AcessoController;
-use App\Http\Controllers\CompromissosController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/compromissos', [CompromissosController::class, 'index'])->name('compromissos');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/compromissos', [CompromissosController::class, 'salvar'])->name('compromissos.salvar');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::put('/compromissos', [CompromissosController::class, 'editar'])->name('compromissos.editar');
-
-Route::get('/compromissos/editar/{compromisso}', [CompromissosController::class, 'editar'])->name('compromissos.editarForm');
-
-Route::delete('/compromissos', [CompromissosController::class, 'apagar'])->name('compromissos.apagar');
-
-Route::get('/acesso', [AcessoController::class, 'index'])->name('acesso');
-
-Route::post('/acesso', [AcessoController::class,'login'])->name('acesso.login');
+require __DIR__.'/auth.php';
